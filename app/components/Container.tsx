47d4f4c874fbc086ec02d406f0fb98649d6a8cdb
Aniruddha Gawali <aniruddhagawali03@gmail.com>
2024-01-11 20:03:26 +0530

    feat : added Montserrat font

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -1,7 +1,7 @@
-import React from "react"
+import React from "react";
 
 const Container = ({ children }: { children: React.ReactNode }) => {
-  return <div className="container mx-auto">{children}</div>
-}
+  return <div className="container mx-auto">{children}</div>;
+};
 
-export default Container
+export default Container;
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,33 +1,37 @@
-import React from "react"
-import Container from "./Container"
-import Link from "next/link"
+import React from "react";
+import Container from "./Container";
+import Link from "next/link";
 
 const Hero = () => {
   return (
     <div className="relative">
       <Container>
-        <div className="flex justify-evenly">
+        <div className="flex  justify-evenly">
           <div className="flex flex-col justify-center pr-4">
             <div className="w-[85%]">
-              <h1 className=" text-5xl font-medium mb-4 leading-[52px]">
+              <h1 className=" mb-4 text-5xl font-semibold leading-[52px]">
                 Empowering Your Brand's Social Media Presence
               </h1>
-              <p className="text-base font-normal text-gray-600 mb-4">
-                We are committed to providing our clients with the highest-quality digital
-                marketing services bro, using the latest technologies to achieve their business
-                goals.
+
+              <p className="mb-4 text-base font-normal text-gray-600">
+                We are committed to providing our clients with the
+                highest-quality digital marketing services bro, using the latest
+                technologies to achieve their business goals.
               </p>
+
               <Link
                 href={"#contact"}
-                className=" inline-flex px-6 py-3 border border-primary rounded-full text-base bg-primary text-white"
+                className=" inline-flex rounded-full border border-primary bg-primary px-6 py-3 text-base text-white"
               >
                 Our Services
               </Link>
             </div>
           </div>
+
           <div className="image">
             <img src="/images/home-illustration.png" alt="hero-image" />
           </div>
+
           <img
             className="absolute -left-0 top-32 w-28"
             src="/images/hero-ellipse.svg"
@@ -36,7 +40,7 @@ const Hero = () => {
         </div>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
